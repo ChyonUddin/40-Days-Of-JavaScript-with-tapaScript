43f59e5f30d9b62f057a1f32bb63dd0fdea46c3b
Bishal <chyonuddin056@gamil.com>
2025-08-26 00:19:23 +0600

    day 2 er variables declaration, reassign, object and array craate kora sikhlam

---
+++ Day 2/task.js
@@ -0,0 +1,36 @@
+/*// ✅ Task 1: Declare variables for a person’s name, age, isStudent status, and favorite programming language.
+✅ Task 2: Print the values to the console.
+✅ Task 3: Try reassigning values to let and const variables and observe errors.
+✅ Task 4: Create an object and an array, assign them to new variables, modify, and observe changes. */
+
+let name = "bishal"; 
+name = "uddin";
+console.log(name);
+let age = 24;
+age = 28;
+console.log(age);
+let isStudent = true;
+isStudent = false;
+console.log(isStudent);
+let favoriteLanguage = "javaScript";
+favoriteLanguage = "python";
+console.log(favoriteLanguage);
+
+// tast 03
+const name1 = "chyon";
+name1 = " it cant be reassign";
+const age1 = 21;
+age1 = " const reassign hoi na";
+const isStudent1 = true;
+const favoriteLanguage1 = "javaScript"
+
+// tast 04
+// creation a object
+let person = {
+    names: "bishal",
+    age: 36,
+    isStuden: true,
+    favoriteLanguage: "JavaScript"
+}
+// creation an array 
+const person1 = [name2,age2,isStudent,favoriteLanguage];
