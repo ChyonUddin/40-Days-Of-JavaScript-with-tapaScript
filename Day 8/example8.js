2dc3ff5a9307525fb656435d435a6176ad237886
Bishal <chyonuddin056@gamil.com>
2025-08-29 12:27:00 +0600

    i have doen the execution contex and save the ss in a  folder

---
+++ Day 8/example8.js
@@ -0,0 +1,18 @@
+console.log("Fundamental is here");
+
+// Lexical Environment
+// # eta diye bujhai code ta kothai place kora ache, like jodi tmi variable er kotha vabo tahole seta ki normal place a ache? or kno block er vitor ache? or kno function er vitor ache? .... ey obostan er upor nirovor kore bolte hoi code ta Lexicaly kothai place hoye ache? like GEC OR FEC? 
+
+/* 
+# GEC Lexicaly place
+
+var name = 'Tom';
+
+function sayName() {
+  console.log(this.name);
+}
+  
+# Jmn ey name variable ta GEC te obostan kache, bcz eta lexicaly place in GEC. Same  sayName() function er jonno . 
+# GEC te inbuild amra window and this pai those are nonpremitive value
+
+*/
